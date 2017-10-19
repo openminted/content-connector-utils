eead49afa88637e44eed6a5cd9de0150ff824e1b
kgiann78 <kgiann78@gmail.com>
2017-10-19 17:30:04 +0300

    - Code refactor: * Add documentType handling in order to distinguish abstract_only and fulltext containing documents * Code cleanup * Though not in this branch, keeping changes made for postgres integration to handle unfinished corpus builds

--- src/main/java/eu/openminted/content/connector/utils/SearchExtensions.java
+++ src/main/java/eu/openminted/content/connector/utils/SearchExtensions.java
@@ -1,13 +1,12 @@
 package eu.openminted.content.connector.utils;
 
-import eu.openminted.content.connector.utils.faceting.OMTDFacetEnum;
 import eu.openminted.content.connector.SearchResult;
 import eu.openminted.registry.core.domain.Facet;
 import eu.openminted.registry.core.domain.Value;
 import org.apache.commons.lang.WordUtils;
 
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.function.BinaryOperator;
 
 public class SearchExtensions {
 
@@ -23,13 +22,44 @@ public class SearchExtensions {
         //TODO: how to merge results? do we need to?
     }
 
+    private static Map<String, Facet> createMap(List<Facet> facetList) {
+        Map<String, Facet> map = new HashMap<>();
+
+        facetList.forEach(f -> {
+            String key = WordUtils.capitalizeFully(f.getField());
+            f.setValues(mergeValues(f));
+            if (!map.containsKey(key)) {
+                map.put(key, f);
+            } else {
+                f.getValues().forEach(v -> map.get(key).getValues().add(v));
+            }
+        });
+        return map;
+    }
+
     private static List<Facet> mergeFacets(List<Facet> f1, List<Facet> f2) {
-//        Map<String, Facet> mf1 = new HashMap<>();
-        if (f1 != null && f2 != null)
-            return new ArrayList<>(mergeFacets(
-                    f1.stream().collect(Collectors.toMap(f -> WordUtils.capitalizeFully(f.getField()), f -> f)),
-                    f2.stream().collect(Collectors.toMap(f -> WordUtils.capitalizeFully(f.getField()), f -> f)))
-                    .values());
+        Map<String, Facet> mf1 = null;
+        Map<String, Facet> mf2 = null;
+
+        if (f1 != null)
+            mf1 = createMap(f1);
+
+        if (f2 != null)
+            mf2 = createMap(f2);
+
+        if (mf1 != null && mf2 != null) {
+            return new ArrayList<>(mergeFacets(mf1, mf2, new BinaryOperator<Facet>() {
+                @Override
+                public Facet apply(Facet facet, Facet facet2) {
+
+
+                    Facet mergedFacet = mergeFacet(facet, facet2);
+                    Collections.sort(mergedFacet.getValues());
+                    Collections.reverse(mergedFacet.getValues());
+                    return mergedFacet;
+                }
+            }).values());
+        }
 
         if (f1 == null && f2 == null) return new ArrayList<>();
 
@@ -37,76 +67,39 @@ public class SearchExtensions {
         else return f1;
     }
 
-    private static Map<String, Facet> mergeFacets(Map<String, Facet> f1, Map<String, Facet> f2) {
-
-        Map<String, Facet> temp = new HashMap<>();
-
-        for (Map.Entry<String, Facet> e : f1.entrySet()) {
-            Facet facet;
-            String keyName = WordUtils.capitalizeFully(e.getKey());
-            facet = buildFacet(e.getValue());
-
-            for (Value value : facet.getValues()) {
-                value.setValue(WordUtils.capitalizeFully(value.getValue()));
-                value.setLabel(WordUtils.capitalizeFully(value.getLabel()));
-            }
-
-            temp.put(keyName, facet);
-        }
-
-        for (Map.Entry<String, Facet> e : f2.entrySet()) {
-            Facet facet;
-            String keyName = WordUtils.capitalizeFully(e.getKey());
-            facet = buildFacet(e.getValue());
-
-            for (Value value : facet.getValues()) {
-                value.setValue(WordUtils.capitalizeFully(value.getValue()));
-                value.setLabel(WordUtils.capitalizeFully(value.getLabel()));
-            }
-
-            if (temp.containsKey(e.getKey())) {
-                temp.put(keyName, mergeFacet(temp.get(keyName), facet));
-            } else {
-                temp.put(keyName, facet);
-            }
-
-            Collections.sort(temp.get(keyName).getValues());
-            Collections.reverse(temp.get(keyName).getValues());
-        }
-
-        return temp;
+    private static Map<String, Facet> mergeFacets(Map<String, Facet> f1, Map<String, Facet> f2, BinaryOperator<Facet> combiner) {
+        f1.forEach((k, v) -> f2.merge(k, v, combiner));
+        return f2;
     }
 
     private static Facet mergeFacet(Facet f1, Facet f2) {
         Facet f = new Facet();
-        Map<String, Integer> temp = new HashMap<>();
-        Map<String, String> labels = new HashMap<>();
+        Map<String, Integer> mapCounts = new HashMap<>();
+        Map<String, String> mapLabels = new HashMap<>();
 
         f.setField(f1.getField());
         f.setLabel(f1.getLabel());
         f.setValues(new ArrayList<>());
 
+
+
         for (Value v : f1.getValues()) {
-            String valueName = WordUtils.capitalizeFully(v.getValue());
-            temp.put(valueName, v.getCount());
-            labels.put(valueName, v.getLabel());
+            mapCounts.put(v.getValue(), v.getCount());
+            mapLabels.put(v.getValue(), v.getLabel());
         }
         for (Value v : f2.getValues()) {
-            String valueName = WordUtils.capitalizeFully(v.getValue());
-            if (temp.containsKey(valueName))
-                temp.put(valueName, v.getCount() + temp.get(WordUtils.capitalizeFully(v.getValue())));
+            if (mapCounts.containsKey(v.getValue()))
+                mapCounts.put(v.getValue(), v.getCount() + mapCounts.get(v.getValue()));
             else {
-                temp.put(valueName, v.getCount());
-                labels.put(valueName, v.getLabel());
+                mapCounts.put(v.getValue(), v.getCount());
+                mapLabels.put(v.getValue(), v.getLabel());
             }
         }
 
-        for (Map.Entry<String, Integer> e : temp.entrySet()) {
+        for (Map.Entry<String, Integer> e : mapCounts.entrySet()) {
             Value v = new Value();
-            String keyName = WordUtils.capitalizeFully(e.getKey());
-
-            v.setValue(keyName);
-            v.setLabel(labels.get(keyName));
+            v.setValue(e.getKey());
+            v.setLabel(mapLabels.get(e.getKey()));
             v.setCount(e.getValue());
 
             f.getValues().add(v);
@@ -115,31 +108,25 @@ public class SearchExtensions {
         return f;
     }
 
-    private static Facet buildFacet(Facet facet) {
-        facet.setValues(mergeValues(facet));
-        return facet;
-    }
-
     private static List<Value> mergeValues(Facet facet) {
-        Map<String, Value> tmpValues = new HashMap<>();
+        Map<String, Value> map = new HashMap<>();
 
-        for (Value value : facet.getValues()) {
-            Value tmpValue = new Value();
-            tmpValue.setCount(value.getCount());
-            tmpValue.setValue(value.getValue());
-            tmpValue.setLabel(value.getLabel());
+        facet.getValues().forEach(v -> {
+            v.setValue(WordUtils.capitalizeFully(v.getValue()));
+            v.setLabel(WordUtils.capitalizeFully(v.getLabel()));
+            String key = v.getValue();
 
-            String keyName = WordUtils.capitalizeFully(tmpValue.getValue());
-
-            if (tmpValues.containsKey(keyName)) {
-                int currentCount = tmpValues.get(keyName).getCount();
-                tmpValues.get(keyName).setCount(currentCount + tmpValue.getCount());
+            if (!map.containsKey(key)) {
+                Value value = new Value(v.getValue(), v.getCount());
+                value.setLabel(v.getLabel());
+                map.put(key, value);
             } else {
-                tmpValues.put(keyName, tmpValue);
+                Value value = map.get(key);
+                value.setCount(value.getCount() + v.getCount());
             }
-        }
+        });
 
-        List<Value> values = new ArrayList<>(tmpValues.values());
+        List<Value> values = new ArrayList<>(map.values());
         Collections.sort(values);
         Collections.reverse(values);
         return values;
--- src/main/java/eu/openminted/content/connector/utils/faceting/OMTDFacetLabels.java
+++ src/main/java/eu/openminted/content/connector/utils/faceting/OMTDFacetLabels.java
@@ -1,5 +1,6 @@
 package eu.openminted.content.connector.utils.faceting;
 
+import eu.openminted.registry.domain.DocumentTypeEnum;
 import eu.openminted.registry.domain.PublicationTypeEnum;
 import eu.openminted.registry.domain.RightsStatementEnum;
 import org.springframework.stereotype.Component;
@@ -9,87 +10,119 @@ import java.util.Map;
 
 @Component
 public class OMTDFacetLabels {
-    private Map<OMTDFacetEnum, String> omtdFacetLabels = new HashMap<>();
-    private Map<RightsStatementEnum, String> omtdRightsStmtLabels = new HashMap<>();
-    private Map<PublicationTypeEnum, String> omtdPublicationTypeLabels = new HashMap<>();
-    private Map<String, PublicationTypeEnum> omtdGetPublicationTypeEnumFromLabel = new HashMap<>();
-    private Map<String, RightsStatementEnum> omtdGetRightsStmtEnumFromLabel = new HashMap<>();
+    private Map<OMTDFacetEnum, String> facetLabelsFromEnum = new HashMap<>();
+
+    private Map<RightsStatementEnum, String> rightsStmtLabelFromEnum = new HashMap<>();
+    private Map<String, RightsStatementEnum> rightsStmtEnumFromLabel = new HashMap<>();
+
+    private Map<PublicationTypeEnum, String> publicationTypeLabelFromEnum = new HashMap<>();
+    private Map<String, PublicationTypeEnum> publicationTypeEnumFromLabel = new HashMap<>();
+
+    private Map<DocumentTypeEnum, String> documentTypeLabelFromEnum = new HashMap<>();
+    private Map<String, DocumentTypeEnum> documentTypeFromLabel = new HashMap<>();
 
 
     public OMTDFacetLabels() {
-        omtdFacetLabels.put(OMTDFacetEnum.PUBLICATION_TYPE, "Publication Type");
-        omtdFacetLabels.put(OMTDFacetEnum.PUBLICATION_YEAR, "Publication Year");
-        omtdFacetLabels.put(OMTDFacetEnum.RIGHTS_STMT_NAME, "Rights Statement");
-        omtdFacetLabels.put(OMTDFacetEnum.RIGHTS, "Rights");
-        omtdFacetLabels.put(OMTDFacetEnum.DOCUMENT_LANG, "Language");
-        omtdFacetLabels.put(OMTDFacetEnum.DOCUMENT_TYPE, "Document Type");
-        omtdFacetLabels.put(OMTDFacetEnum.SOURCE, "Content Source");
+        facetLabelsFromEnum.put(OMTDFacetEnum.PUBLICATION_TYPE, "Publication Type");
+        facetLabelsFromEnum.put(OMTDFacetEnum.PUBLICATION_YEAR, "Publication Year");
+        facetLabelsFromEnum.put(OMTDFacetEnum.RIGHTS_STMT_NAME, "Rights Statement");
+        facetLabelsFromEnum.put(OMTDFacetEnum.RIGHTS, "Rights");
+        facetLabelsFromEnum.put(OMTDFacetEnum.DOCUMENT_LANG, "Language");
+        facetLabelsFromEnum.put(OMTDFacetEnum.DOCUMENT_TYPE, "Document Type");
+        facetLabelsFromEnum.put(OMTDFacetEnum.SOURCE, "Content Source");
 
-        omtdRightsStmtLabels.put(RightsStatementEnum.OPEN_ACCESS, "Open Access");
-        omtdRightsStmtLabels.put(RightsStatementEnum.RESTRICTED_ACCESS, "Restricted Access");
+        /* rightsStmt Labels */
+        rightsStmtLabelFromEnum.put(RightsStatementEnum.OPEN_ACCESS, "Open Access");
+        rightsStmtLabelFromEnum.put(RightsStatementEnum.RESTRICTED_ACCESS, "Restricted Access");
 
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.ANNOTATION, "Annotation");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.RESEARCH_ARTICLE, "Research Article");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.RESEARCH_PROPOSAL, "Research Proposal");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.DOCTORAL_THESIS, "Doctoral Thesis");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.THESIS, "Thesis");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.CONFERENCE_OBJECT, "Conference Object");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.PRE_PRINT, "Pre Print");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.RESEARCH_REPORT, "Research Report");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.BOOK, "Book");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.MASTER_THESIS, "Master Thesis");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.BOOK_PART, "Book Part");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.REPORT, "Report");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.REVIEW, "Review");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.BACHELOR_THESIS, "Bachelor Thesis");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.LECTURE, "Lecture");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.PATENT, "Patent");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.CONTRIBUTION_TO_JOURNAL, "Contribution to Journal");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.DATA_PAPER, "Data Paper");
-        omtdPublicationTypeLabels.put(PublicationTypeEnum.OTHER, "Other");
+        /* rightsStmtEnum From Label */
+        rightsStmtEnumFromLabel.put("Open Access", RightsStatementEnum.OPEN_ACCESS);
+        rightsStmtEnumFromLabel.put("Restricted Access", RightsStatementEnum.RESTRICTED_ACCESS);
 
-        omtdGetPublicationTypeEnumFromLabel.put("Annotation", PublicationTypeEnum.ANNOTATION);
-        omtdGetPublicationTypeEnumFromLabel.put("Research Article", PublicationTypeEnum.RESEARCH_ARTICLE);
-        omtdGetPublicationTypeEnumFromLabel.put("Research Proposal", PublicationTypeEnum.RESEARCH_PROPOSAL);
-        omtdGetPublicationTypeEnumFromLabel.put("Thesis", PublicationTypeEnum.THESIS);
-        omtdGetPublicationTypeEnumFromLabel.put("Doctoral Thesis", PublicationTypeEnum.DOCTORAL_THESIS);
-        omtdGetPublicationTypeEnumFromLabel.put("Conference Object", PublicationTypeEnum.CONFERENCE_OBJECT);
-        omtdGetPublicationTypeEnumFromLabel.put("Pre Print", PublicationTypeEnum.PRE_PRINT);
-        omtdGetPublicationTypeEnumFromLabel.put("Research Report", PublicationTypeEnum.RESEARCH_REPORT);
-        omtdGetPublicationTypeEnumFromLabel.put("Book", PublicationTypeEnum.BOOK);
-        omtdGetPublicationTypeEnumFromLabel.put("Master Thesis", PublicationTypeEnum.MASTER_THESIS);
-        omtdGetPublicationTypeEnumFromLabel.put("Book Part", PublicationTypeEnum.BOOK_PART);
-        omtdGetPublicationTypeEnumFromLabel.put("Report", PublicationTypeEnum.REPORT);
-        omtdGetPublicationTypeEnumFromLabel.put("Review", PublicationTypeEnum.REVIEW);
-        omtdGetPublicationTypeEnumFromLabel.put("Bachelor Thesis", PublicationTypeEnum.BACHELOR_THESIS);
-        omtdGetPublicationTypeEnumFromLabel.put("Lecture", PublicationTypeEnum.LECTURE);
-        omtdGetPublicationTypeEnumFromLabel.put("Patent", PublicationTypeEnum.PATENT);
-        omtdGetPublicationTypeEnumFromLabel.put("Contribution to Journal", PublicationTypeEnum.CONTRIBUTION_TO_JOURNAL);
-        omtdGetPublicationTypeEnumFromLabel.put("Data Paper", PublicationTypeEnum.DATA_PAPER);
-        omtdGetPublicationTypeEnumFromLabel.put("Other", PublicationTypeEnum.OTHER);
+        /* publicationType Labels */
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.ANNOTATION, "Annotation");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.RESEARCH_ARTICLE, "Research Article");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.RESEARCH_PROPOSAL, "Research Proposal");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.DOCTORAL_THESIS, "Doctoral Thesis");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.THESIS, "Thesis");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.CONFERENCE_OBJECT, "Conference Object");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.PRE_PRINT, "Pre Print");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.RESEARCH_REPORT, "Research Report");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.BOOK, "Book");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.MASTER_THESIS, "Master Thesis");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.BOOK_PART, "Book Part");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.REPORT, "Report");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.REVIEW, "Review");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.BACHELOR_THESIS, "Bachelor Thesis");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.LECTURE, "Lecture");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.PATENT, "Patent");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.CONTRIBUTION_TO_JOURNAL, "Contribution to Journal");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.DATA_PAPER, "Data Paper");
+        publicationTypeLabelFromEnum.put(PublicationTypeEnum.OTHER, "Other");
 
-        omtdGetRightsStmtEnumFromLabel.put("Open Access", RightsStatementEnum.OPEN_ACCESS);
-        omtdGetRightsStmtEnumFromLabel.put("Restricted Access", RightsStatementEnum.RESTRICTED_ACCESS);
+        /* publicationType from Labels */
+        publicationTypeEnumFromLabel.put("Annotation", PublicationTypeEnum.ANNOTATION);
+        publicationTypeEnumFromLabel.put("Research Article", PublicationTypeEnum.RESEARCH_ARTICLE);
+        publicationTypeEnumFromLabel.put("Research Proposal", PublicationTypeEnum.RESEARCH_PROPOSAL);
+        publicationTypeEnumFromLabel.put("Thesis", PublicationTypeEnum.THESIS);
+        publicationTypeEnumFromLabel.put("Doctoral Thesis", PublicationTypeEnum.DOCTORAL_THESIS);
+        publicationTypeEnumFromLabel.put("Conference Object", PublicationTypeEnum.CONFERENCE_OBJECT);
+        publicationTypeEnumFromLabel.put("Pre Print", PublicationTypeEnum.PRE_PRINT);
+        publicationTypeEnumFromLabel.put("Research Report", PublicationTypeEnum.RESEARCH_REPORT);
+        publicationTypeEnumFromLabel.put("Book", PublicationTypeEnum.BOOK);
+        publicationTypeEnumFromLabel.put("Master Thesis", PublicationTypeEnum.MASTER_THESIS);
+        publicationTypeEnumFromLabel.put("Book Part", PublicationTypeEnum.BOOK_PART);
+        publicationTypeEnumFromLabel.put("Report", PublicationTypeEnum.REPORT);
+        publicationTypeEnumFromLabel.put("Review", PublicationTypeEnum.REVIEW);
+        publicationTypeEnumFromLabel.put("Bachelor Thesis", PublicationTypeEnum.BACHELOR_THESIS);
+        publicationTypeEnumFromLabel.put("Lecture", PublicationTypeEnum.LECTURE);
+        publicationTypeEnumFromLabel.put("Patent", PublicationTypeEnum.PATENT);
+        publicationTypeEnumFromLabel.put("Contribution to Journal", PublicationTypeEnum.CONTRIBUTION_TO_JOURNAL);
+        publicationTypeEnumFromLabel.put("Data Paper", PublicationTypeEnum.DATA_PAPER);
+        publicationTypeEnumFromLabel.put("Other", PublicationTypeEnum.OTHER);
 
+        /* documentType Labels */
+        documentTypeLabelFromEnum.put(DocumentTypeEnum.WITH_ABSTRACT_ONLY, "Abstract");
+        documentTypeLabelFromEnum.put(DocumentTypeEnum.WITH_FULL_TEXT, "Fulltext");
+
+        /* documentType From Labels */
+        documentTypeFromLabel.put("Abstract", DocumentTypeEnum.WITH_ABSTRACT_ONLY);
+        documentTypeFromLabel.put("Fulltext", DocumentTypeEnum.WITH_FULL_TEXT);
     }
 
-    public Map<OMTDFacetEnum, String> getOmtdFacetLabels() {
-        return omtdFacetLabels;
+    public String getFacetLabelsFromEnum(OMTDFacetEnum omtdFacetEnum) {
+        if (facetLabelsFromEnum.containsKey(omtdFacetEnum))
+            return facetLabelsFromEnum.get(omtdFacetEnum);
+        return "";
     }
 
-    public Map<RightsStatementEnum, String> getOmtdRightsStmtLabels() {
-        return omtdRightsStmtLabels;
+    public String getRightsStmtLabelFromEnum(RightsStatementEnum rightsStatementEnum) {
+        if (rightsStmtLabelFromEnum.containsKey(rightsStatementEnum))
+            return rightsStmtLabelFromEnum.get(rightsStatementEnum);
+        return "";
     }
 
-    public Map<PublicationTypeEnum, String> getOmtdPublicationTypeLabels() {
-        return omtdPublicationTypeLabels;
+    public Map<String, RightsStatementEnum> getRightsStmtEnumFromLabel() {
+        return rightsStmtEnumFromLabel;
     }
 
-    public Map<String, PublicationTypeEnum> getOmtdGetPublicationTypeEnumFromLabel() {
-        return omtdGetPublicationTypeEnumFromLabel;
+    public String getPublicationTypeLabelFromEnum(PublicationTypeEnum publicationTypeEnum) {
+        if (publicationTypeLabelFromEnum.containsKey(publicationTypeEnum))
+            return publicationTypeLabelFromEnum.get(publicationTypeEnum);
+        return "";
     }
 
-    public Map<String, RightsStatementEnum> getOmtdGetRightsStmtEnumFromLabel() {
-        return omtdGetRightsStmtEnumFromLabel;
+    public Map<String, PublicationTypeEnum> getPublicationTypeEnumFromLabel() {
+        return publicationTypeEnumFromLabel;
+    }
+
+    public String getDocumentTypeLabelFromEnum(DocumentTypeEnum documentTypeEnum) {
+        if (documentTypeLabelFromEnum.containsKey(documentTypeEnum))
+            return documentTypeLabelFromEnum.get(documentTypeEnum);
+        return "";
+    }
+
+    public Map<String, DocumentTypeEnum> getDocumentTypeFromLabel() {
+        return documentTypeFromLabel;
     }
 }
